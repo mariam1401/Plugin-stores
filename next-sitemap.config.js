74b1a5d818e98d2db7ec44e1904d87f625061e2e
Mariam <mariamkarapetyan1401@gmail.com>
2024-10-10 15:21:00 +0400

    change sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,16 @@
 /** @type {import('next-sitemap').IConfig} */
 
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://10web.io/',
+  siteUrl: process.env.SITE_URL || 'https://10web.io/wordpress-plugins',
+  changefreq: 'daily',
+  priority: 0.7,
+  generateIndexSitemap: true,
+  transform: async (config, path) => {
+    return {
+      loc: `https://10web.io/${path}`,
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+    };
+  },
 };
