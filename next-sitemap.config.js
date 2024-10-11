2f0086e8cb255791c13f5fb0e97341af99a7daf3
Mariam <mariamkarapetyan1401@gmail.com>
2024-10-11 11:26:23 +0400

    update url

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://10web.io/wordpress-plugins',
+  siteUrl: process.env.SITE_URL || 'https://10web.io/wordpress-plugins/',
   changefreq: 'daily',
   priority: 0.7,
   generateIndexSitemap: true,
